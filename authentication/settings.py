ce954d4493d7b0a622b97c2137da0827b9edb8ab
Priya Dugg <146826459+pria-pria@users.noreply.github.com>
2023-12-04 22:32:12 +0530

    Update settings.py

--- authentication/settings.py
+++ authentication/settings.py
@@ -19,11 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g-&bs3=+x9(g*#jm*1g*92h%*ng56!fnv4&9g!&e*)93n30puq'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
